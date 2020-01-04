81f289205d2c35fade19fd6e2cc9b3f0bdff2628
wsm8855 <=>
2020-01-04 17:32:45 -0500

    Cleaned up documentation and added additional output to exception handling in update_observers()

--- sudoku/model.py
+++ sudoku/model.py
@@ -1,4 +1,22 @@
-def print_board(board):
+"""
+model.py
+
+A module containing the Model class and the print_board function.
+
+author: Wiley Matthews
+"""
+import sys
+from typing import List
+
+from view import View
+
+
+def print_board(board: List[List[str]]) -> None:
+    """
+    Prints a soduku puzzle to standard output.
+    :param board: nested lists representing the puzzle
+    :return: None
+    """
     for i in board:
         for j in i:
             if j == '.':
@@ -10,22 +28,43 @@ def print_board(board):
 
 class Model(object):
 
-    def __init__(self, start_board):
+    def __init__(self, start_board: List[List[str]]) -> None:
+        """
+        Model that represents the state and operation of a sudoku puzzle.
+        :param start_board: nested lists representing the initial puzzle state
+        """
         self.board = start_board
         self.observers = []
 
-    def add_observer(self, obs):
+    def add_observer(self, obs: View) -> None:
+        """
+        Add a view as an observer of this model.
+        :param obs: view to observe this model.
+        :return: None
+        """
         self.observers.append(obs)
 
-    def make_move(self, new_char, i, j):
+    def make_move(self, new_char: str, i: int, j: int) -> None:
+        """
+        Change a character in the puzzle. (coordinates in i,j list notation, not x,y notation.)
+        :param new_char: new value of specified index.
+        :param i: i coordinate of change
+        :param j: j coordinate of change
+        :return: None
+        """
         self.board[i][j] = new_char
         self.update_observers()
 
-    def update_observers(self):
-        # print("Update")
-        #         # print_board(self.board)
+    def update_observers(self) -> None:
+        """
+        Update any/all observers of this model that a state change has occurred.
+        :return: None
+        """
         try:
             for observer in self.observers:
                 observer.update_state()
-        except:
+        except Exception as e:
+            sys.stderr.write("Suppressed exception in model.")
+            print(e)
+            sys.stderr.write("Exiting program.")
             exit()
